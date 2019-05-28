3bb11c942bab328afea1553158480a2f7de62af1
Ratandeep Singh <ratan14@gmail.com>
2019-05-29 00:32:38 +0530

    Added previous and next, and changed font

--- src/DailyPage.js
+++ src/DailyPage.js
@@ -16,11 +16,64 @@ class DailyPage extends React.Component {
         console.log("error", err);
       });
   }
+  goBack = () => {
+    window.location = window.location.origin + "/" + this.state.daily.previous;
+  };
+  goForward = () => {
+    window.location = window.location.origin + "/" + this.state.daily.next;
+  };
   render() {
     return (
       <div>
-        <div style={{ fontSize: 42, width: "100%", justifyContent: "center" }}>
+        <div
+          style={{
+            fontSize: 42,
+            width: "100%",
+            justifyContent: "center",
+            padding: 16,
+            fontFamily: "League Spartan"
+          }}
+        >
           {this.state.daily.heading}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            paddingLeft: 16
+          }}
+        >
+          {this.state.daily.previous &&
+            this.state.daily.previous != "" && (
+              <div
+                onClick={this.goBack}
+                style={{
+                  padding: 16,
+                  justifyContent: "center",
+                  fontFamily: "League Spartan",
+                  backgroundColor: "#ffdd59",
+                  borderRadius: 5
+                }}
+              >
+                Previous
+              </div>
+            )}
+          {this.state.daily.next &&
+            this.state.daily.next != "" && (
+              <div
+                onClick={this.goForward}
+                style={{
+                  padding: 16,
+                  marginLeft: 16,
+                  justifyContent: "center",
+                  fontFamily: "League Spartan",
+                  backgroundColor: "#ffdd59",
+                  borderRadius: 5
+                }}
+              >
+                Next
+              </div>
+            )}
         </div>
         <img style={{ width: "100%" }} src={this.state.daily.photo_link} />
       </div>
